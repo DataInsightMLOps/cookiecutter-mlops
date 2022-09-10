5c5c7a0dcf8ad8e71f95a67d997ae2dae5a1814c
akshatthakar <akshat.thakar@gmail.com>
2022-09-10 09:51:33 +0530

    Update __init__.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils/__init__.py
@@ -1,3 +1,12 @@
-from .saving import log_path, trainer_paths
-from .visualization import TensorboardWriter
-from .logger import setup_logger, setup_logging
+from utils.pylogger import get_pylogger
+from utils.rich_utils import enforce_tags, print_config_tree
+from utils.utils import (
+    close_loggers,
+    extras,
+    get_metric_value,
+    instantiate_callbacks,
+    instantiate_loggers,
+    log_hyperparameters,
+    save_file,
+    task_wrapper,
+)
